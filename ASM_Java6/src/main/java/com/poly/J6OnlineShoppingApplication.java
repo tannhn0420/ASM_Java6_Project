c89c455487c162caa55e0c0d3dcac6e63e6f4ffa
tannhn0420 <tannhn0420@gmail.com>
2021-07-23 23:31:40 +0700

    #111 create new project java6

---
+++ ASM_Java6/src/main/java/com/poly/J6OnlineShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J6OnlineShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J6OnlineShoppingApplication.class, args);
+    }
+
+}
